2d59c76764502f350c24ab8c304c99d0a0e35235
Jack S. Hale <mail@jackhale.co.uk>
2021-04-15 11:53:09 +0200

    Fix.

--- src/aws.js
+++ src/aws.js
@@ -13,6 +13,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     'curl -O -L https://github.com/actions/runner/releases/download/v2.277.1/actions-runner-linux-arm64-2.277.1.tar.gz',
     'tar xzf ./actions-runner-linux-arm64-2.277.1.tar.gz',
     'export RUNNER_ALLOW_RUNASROOT=1',
+    'export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1',
     `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
     './run.sh',
   ];
